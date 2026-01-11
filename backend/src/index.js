9587c878ec7cd9ec2157112cfd642485c0b1cede
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-01-11 11:19:46 +0600

    Server is ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res) => {
-    res.send("Hey there");
+app.get("/", (req, res) => {
+    res.send("Hey there")
 });
 
+app.get("facebook", (req, res) => {
+    res.send("Face book here")
+})
 
 
-app.listen()
+
+app.listen(PORT, () => {
+    console.log(`App is running in http://localhost:${PORT}`);
+})
